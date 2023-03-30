7d15a868720c69d33eb0f291799ca6ae895ca893
Geeraz <13kreso@gmail.com>
2023-03-31 00:22:52 +0200

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        darkbg: "#1E293B",
-      },
-      screens: {
-        other: { min: "340px", max: "1265px" },
+    content: ["./src/**/*.{html,js}"],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        screens: {
+          "other": {'min': '340px', 'max': '1200px'},
+        },
+        colors: {
+          darkbg: "#1E293B",
+          blue: {
+            850: "#1e40af"
+          }
+        }
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
