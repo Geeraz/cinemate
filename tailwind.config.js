a6638086d2107f517d95e79bde6cae17ff6777ac
Geeraz <13kreso@gmail.com>
2023-05-21 15:05:04 +0200

    Porfolio changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    darkMode: 'class',
-    theme: {
-      extend: {
-        screens: {
-          "other": {'min': '340px', 'max': '1200px'},
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      screens: {
+        other: { min: "340px", max: "1200px" },
+      },
+      colors: {
+        darkbg: "#1E293B",
+        grey1: "#ced4da",
+        grey2: "#adb5bd",
+        grey3: "#6c757d",
+        grey4: "#495057",
+        grey5: "#343a40",
+        grey6: "#212529",
+        prim: "#FFBF3F",
+        blue: {
+          850: "#1e40af",
         },
-        colors: {
-          darkbg: "#1E293B",
-          blue: {
-            850: "#1e40af"
-          }
-        }
       },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
